c7dcf34af0296e8edd7bba9dfdbfc780c7c2a6dc
nor3blk <50574633+nor3blk@users.noreply.github.com>
2021-03-03 09:06:53 +0900

    Update workflow and action

--- .github/actions/ccs930-build/entrypoint.sh
+++ .github/actions/ccs930-build/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+DATA=/github
+PJ=workspace
+PJ_HOME=${DATA}/${PJ}
+OUT_DIR=${PJ_HOME}/artifact
+
 config_list=$1
 echo $config_list
 configs=(${config_list//,/ })
 echo ${configs[@]}
 
 for config in ${configs[@]}; do
-    eclipse -noSplash -data /github -application com.ti.ccstudio.apps.projectBuild -ccs.autoImport -ccs.projects workspace -ccs.configuration ${config}
+    eclipse -noSplash -data ${DATA} -application com.ti.ccstudio.apps.projectBuild -ccs.autoImport -ccs.projects ${PJ} -ccs.configuration ${config}
+    mkdir -p ${OUT_DIR}/${config}
+    cp -p ${PJ_HOME}/${config}/${PJ}.{out,map} ${OUT_DIR}/${config}/
+    head -16 ${PJ_HOME}/${config}/${PJ}.map | tail -7 > ${OUT_DIR}/${config}/${PJ}.txt
 done
